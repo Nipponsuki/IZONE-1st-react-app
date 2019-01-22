a505b6ff2ed3a4b6d92596c31138b93a5e7058dd
Alex <alex.alexeev23061991@gmail.com>
2019-01-22 15:21:56 +0300

    first

---
+++ build/precache-manifest.4eb2d617c9889be229371a6cc56ae9b3.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "481db99a01658c30b0a5369a49371a80",
+    "url": "/Nipponsuki/IZONE-1st-react-app.git/static/media/slide_two.481db99a.jpg"
+  },
+  {
+    "revision": "5b20b485bf9e360fbdea44a200e6e591",
+    "url": "/Nipponsuki/IZONE-1st-react-app.git/static/media/slide_three.5b20b485.jpg"
+  },
+  {
+    "revision": "c696ef163f173dfb197f22d2f1ce0d2f",
+    "url": "/Nipponsuki/IZONE-1st-react-app.git/static/media/slide_one.c696ef16.jpg"
+  },
+  {
+    "revision": "da6f4b039845bcb42c74e79f04293203",
+    "url": "/Nipponsuki/IZONE-1st-react-app.git/static/media/logo.da6f4b03.png"
+  },
+  {
+    "revision": "d88dbee64243776e3360",
+    "url": "/Nipponsuki/IZONE-1st-react-app.git/static/js/runtime~main.d88dbee6.js"
+  },
+  {
+    "revision": "183e9f402b11ba8c364a",
+    "url": "/Nipponsuki/IZONE-1st-react-app.git/static/js/main.183e9f40.chunk.js"
+  },
+  {
+    "revision": "9e86ee27f5c306d14b31",
+    "url": "/Nipponsuki/IZONE-1st-react-app.git/static/js/1.9e86ee27.chunk.js"
+  },
+  {
+    "revision": "183e9f402b11ba8c364a",
+    "url": "/Nipponsuki/IZONE-1st-react-app.git/static/css/main.9d05a7ea.chunk.css"
+  },
+  {
+    "revision": "644f5206779f180891aaafe41a819a1d",
+    "url": "/Nipponsuki/IZONE-1st-react-app.git/index.html"
+  }
+];
